65bd4d81176ee2248fa7da91f9ba735521956bcd
twix <91138333+whichtwix@users.noreply.github.com>
2022-12-24 14:16:14 +0600

    Simplify switch and assign dummies roles

--- source/Patches/Roles/Role.cs
+++ source/Patches/Roles/Role.cs
@@ -348,7 +348,7 @@ namespace TownOfUs.Roles
         public static Role GetRole(PlayerVoteArea area)
         {
             var player = PlayerControl.AllPlayerControls.ToArray()
-                .FirstOrDefault(x => x.PlayerId == area.TargetPlayerId);
+                .FirstOrDefault(x => x.PlayerId == area.TargetPlayerId && !x.isDummy);
             return player == null ? null : GetRole(player);
         }
 
@@ -734,7 +734,7 @@ namespace TownOfUs.Roles
                     }
 
                     var role = GetRole(player);
-                    if (role != null)
+                    if (role != null && !player.isDummy)
                         if (role.Criteria())
                         {
 
